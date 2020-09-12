6fd4309dfd9435cf4e3edf0ef93a7ebda401c5af
aryateja1996 <aryateja.nsa2@gmail.com>
2020-09-12 09:24:47 +0530

    helo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WorkComponent } from './work/work.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {  path: 'work', component: WorkComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
